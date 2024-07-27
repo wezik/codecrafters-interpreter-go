f53b5ce24144e2419a51f5035d7fb9648ad8042b
wezik <wezik@users.noreply.github.com>
2024-07-27 13:55:18 +0200

    Braces and tokenization

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -5,6 +5,20 @@ import (
 	"os"
 )
 
+type LexToken struct {
+	tokenType string
+	lexeme    string
+	literal   string
+}
+
+func newToken(tokenType string, lexeme string, literal string) LexToken {
+	return LexToken{tokenType, lexeme, literal}
+}
+
+func newTokenNoLit(tokenType string, lexeme string) LexToken {
+	return LexToken{tokenType, lexeme, "null"}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
@@ -21,18 +35,33 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Uncomment this block to pass the first stage
-	//
-	// filename := os.Args[2]
-	// fileContents, err := os.ReadFile(filename)
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// if len(fileContents) > 0 {
-	// 	panic("Scanner not implemented")
-	// } else {
-	// 	fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
-	// }
+	filename := os.Args[2]
+	fileContents, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	tokens := []LexToken{}
+
+	if len(fileContents) > 0 {
+		for _, b := range fileContents {
+			if b == '{' {
+				tokens = append(tokens, newTokenNoLit("LEFT_BRACE", "{"))
+			} else if b == '}' {
+				tokens = append(tokens, newTokenNoLit("RIGHT_BRACE", "}"))
+			} else if b == '(' {
+				tokens = append(tokens, newTokenNoLit("LEFT_PAREN", "("))
+			} else if b == ')' {
+				tokens = append(tokens, newTokenNoLit("RIGHT_PAREN", ")"))
+			}
+		}
+		tokens = append(tokens, newTokenNoLit("EOF", ""))
+	} else {
+		tokens = append(tokens, newTokenNoLit("EOF", ""))
+	}
+
+	for _, t := range tokens {
+		fmt.Printf("%s %s %s\n", t.tokenType, t.lexeme, t.literal)
+	}
 }
