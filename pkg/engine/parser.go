062912b6ebad98ee875582ee341654c07ef97a3f
wezik <wezik@users.noreply.github.com>
2024-08-07 21:16:28 +0200

    Parser missing implementation

--- pkg/engine/parser.go
+++ pkg/engine/parser.go
@@ -1,12 +1,36 @@
 package engine
 
+import "slices"
+
 type LexAST struct {
+	//TODO
+	value string
 }
 
 func (a LexAST) String() string {
-	return "TODO"
+	//TODO
+	return a.value
+}
+
+var tokenToLexeme = []string{
+	TOKEN_TRUE,
+	TOKEN_FALSE,
+	TOKEN_NIL,
+}
+
+var tokenToLiteral = []string{
+	TOKEN_NUMBER,
+	TOKEN_STRING,
 }
 
 func Parse(lexTokens []LexToken) ([]LexAST, []error) {
-	return nil, nil
+	var ast []LexAST
+	for _, t := range lexTokens {
+		if slices.Contains(tokenToLexeme, t.TokenType) {
+			ast = append(ast, LexAST{t.Lexeme})
+		} else if slices.Contains(tokenToLiteral, t.TokenType) {
+			ast = append(ast, LexAST{t.Literal})
+		}
+	}
+	return ast, nil
 }
