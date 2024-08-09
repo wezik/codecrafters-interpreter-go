5e838381f66257502e7a0a17a28f4ee081ac65e0
wezik <wezik@users.noreply.github.com>
2024-08-09 04:10:42 +0200

    Doing parsers as god intended

--- pkg/engine/parser.go
+++ pkg/engine/parser.go
@@ -2,7 +2,6 @@ package engine
 
 import (
 	"fmt"
-	"strings"
 )
 
 type Expr interface {
@@ -18,189 +17,156 @@ func (e ExprLiteral) String() string {
 	switch tokenType {
 	case TOKEN_NUMBER, TOKEN_STRING:
 		return e.LexToken.Literal
-	case TOKEN_TRUE, TOKEN_FALSE:
-		return e.LexToken.Lexeme
-	case TOKEN_NIL:
+	default:
 		return e.LexToken.Lexeme
 	}
-	return "nil"
+}
+
+type ExprGroup struct {
+	expr Expr
+}
+
+func (e *ExprGroup) String() string {
+	return fmt.Sprintf("(group %s)", e.expr)
 }
 
 type ExprUnary struct {
 	LexToken LexToken
-	Expr     Expr
+	Expr     *Expr
 }
 
-func (e ExprUnary) String() string {
-	return fmt.Sprintf("(%s %s)", e.LexToken.Lexeme, e.Expr.String())
+func (e *ExprUnary) String() string {
+	return fmt.Sprintf("(%s %s)", e.LexToken.Lexeme, (*e.Expr).String())
 }
 
-type ExprGroup struct {
-	Exprs  []Expr
-	Parent *ExprGroup
+type ExprBinary struct {
+	Left     *Expr
+	Operator LexToken
+	Right    *Expr
 }
 
-func (e *ExprGroup) String() string {
-	var exprs []string
-	for _, expr := range e.Exprs {
-		exprs = append(exprs, expr.String())
-	}
-	if e.Parent == nil {
-		return strings.Join(exprs, " ")
-	}
-	return fmt.Sprintf("(group %s)", strings.Join(exprs, " "))
-}
-
-type ParserState struct {
-	errs []error
-	globalGroup *ExprGroup
-	currentGroup *ExprGroup
-	readIndex int
+type Parser struct {
 	tokens []LexToken
+	tokensIndex int
 }
 
-// func Parse
-//
-// func Parse(lexTokens []LexToken) ([]Expr, []error) {
-// 	var errs []error
-// 	globalGroup := &ExprGroup{Exprs: []Expr{}, Parent: nil}
-// 	currentGroup := globalGroup
-//
-// 	for lexI := 0; lexI < len(lexTokens); lexI++ {
-// 		lexT := lexTokens[lexI]
-// 		switch lexT.TokenType {
-// 		case TOKEN_LEFT_PAREN:
-// 			newGroup := ExprGroup{Exprs: []Expr{}, Parent: currentGroup}
-// 			currentGroup.Exprs = append(currentGroup.Exprs, &newGroup)
-// 			currentGroup = &newGroup
-// 		case TOKEN_RIGHT_PAREN:
-// 			if currentGroup.Parent == nil {
-// 				errs = append(errs, fmt.Errorf("Error: Unmatched parentheses."))
-// 				continue
-// 			} else if len(currentGroup.Exprs) == 0 {
-// 				currentGroup.Parent.Exprs = []Expr{}
-// 				errs = append(errs, fmt.Errorf("Error: Empty group."))
-// 			}
-// 			currentGroup = currentGroup.Parent
-// 		case TOKEN_NUMBER, TOKEN_STRING, TOKEN_TRUE, TOKEN_FALSE, TOKEN_NIL:
-// 			currentGroup.Exprs = append(currentGroup.Exprs, ExprLiteral{lexT})
-// 		case TOKEN_MINUS, TOKEN_BANG:
-// 			if lexI < len(lexTokens)-1 {
-// 				nextLexT := lexTokens[lexI+1]
-// 				currentGroup.Exprs = append(currentGroup.Exprs, ExprUnary{lexT, ExprLiteral{nextLexT}})
-// 				lexI++
-// 				continue
-// 			}
-// 			errs = append(errs, fmt.Errorf("Error: Unexpected token: %s", lexT.Lexeme))
-// 		}
-// 	}
-// 	if currentGroup.Parent != nil {
-// 		errs = append(errs, fmt.Errorf("Error: Unmatched parentheses."))
-// 		currentGroup.Parent.Exprs = []Expr{}
-// 	}
-// 	return globalGroup.Exprs, errs
-// }
-//
-// var tokenExpressions = map[string]func(*ParserState) Expr{}
-//
-// func init() {
-// 	tokenExpressions = map[string]func(*ParserState) Expr{
-// 		TOKEN_NUMBER: parseLiteral,
-// 		TOKEN_STRING: parseLiteral,
-// 		TOKEN_TRUE: parseLiteral,
-// 		TOKEN_FALSE: parseLiteral,
-// 		TOKEN_NIL: parseLiteral,
-// 		TOKEN_MINUS: parseUnary,
-// 		TOKEN_BANG: parseUnary,
-// 		TOKEN_LEFT_PAREN: parseNewGroup,
-// 	}
-// }
+func (p *Parser) current() LexToken {
+	return p.tokens[p.tokensIndex]
+}
 
-// func Parse(lexTokens []LexToken) ([]Expr, []error) {
-// 	state := ParserState{
-// 		errs: []error{},
-// 		globalGroup: &ExprGroup{Exprs: []Expr{}, Parent: nil},
-// 		readIndex: 0,
-// 		tokens: lexTokens,
-// 	}
-// 	state.currentGroup = state.globalGroup
-//
-// 	for ; state.readIndex < len(state.tokens); state.readIndex++ {
-// 		lexT := state.tokens[state.readIndex]
-// 		exprs := &state.currentGroup.Exprs
-// 		expr, err := processToken(&state, lexT)
-// 		if err != nil {
-// 			state.errs = append(state.errs, err)
-// 			continue
-// 		}
-// 		if expr == nil {
-// 			continue
-// 		}
-// 		*exprs = append(*exprs, expr)
-// 	}
-//
-// 	return state.currentGroup.Exprs, state.errs
-// }
-//
-// func processToken(state *ParserState, lexT LexToken) (Expr, error) {
-// 	exprs := &state.currentGroup.Exprs
-// 	switch lexT.TokenType {
-// 	case TOKEN_EOF:
-// 		break
-// 	case TOKEN_NUMBER, TOKEN_STRING, TOKEN_TRUE, TOKEN_FALSE, TOKEN_NIL:
-// 		*exprs = append(*exprs, ExprLiteral{lexT})
-// 	case TOKEN_MINUS, TOKEN_BANG:
-// 		parseUnary(state)
-// 	case TOKEN_LEFT_PAREN:
-// 		parseGroup(state), nil
-// 	case TOKEN_RIGHT_PAREN:
-// 		return 
-// 	}
-// 	return nil, fmt.Errorf("Error: Unexpected token: %s", lexT.Lexeme)
-// }
-//
-// func parseGroup(state *ParserState) Expr {
-// 	group := ExprGroup{Exprs: []Expr{}, Parent: state.currentGroup}
-// 	state.currentGroup.Exprs = append(state.currentGroup.Exprs, &group)
-// 	state.currentGroup = &group
-// 	return &group
-// }
-//
-// func parseGroup(state *ParserState) (*ExprGroup, error) {
-// 	newGroup := ExprGroup{Exprs: []Expr{}, Parent: state.currentGroup}
-// 	state.currentGroup = &newGroup
-// 	for ; state.readIndex < len(state.tokens); state.readIndex++ {
-// 		lexT := state.tokens[state.readIndex]
-// 		expr, err := processToken(state, lexT)
-// 		if err != nil {
-// 			state.errs = append(state.errs, err)
-// 			continue
-// 		}
-// 		state.currentGroup.Exprs = append(state.currentGroup.Exprs, expr)
-// 	}
-// 	state.currentGroup = state.currentGroup.Parent
-// 	return &newGroup, nil
-// }
-//
-//
-// func parseUnary(state *ParserState) error {
-// 	exprUnary := ExprUnary{state.tokens[state.readIndex], nil}
-// 	state.readIndex++
-// 	nextLexT := state.tokens[state.readIndex]
-// 	expr, err := processToken(state, nextLexT)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	exprUnary.Expr = expr
-//
-// }
+func (p *Parser) previous() LexToken {
+	return p.tokens[p.tokensIndex-1]
+}
+
+func (p *Parser) advance() LexToken {
+	if !p.isAtEnd() {
+		p.tokensIndex++
+	}
+	return p.previous()
+}
+
+func (p *Parser) check(tokenType string) bool {
+	return p.current().TokenType == tokenType
+}
+
+func (p *Parser) match(tokenTypes ...string) bool {
+	for _, tokenType := range tokenTypes {
+		if p.check(tokenType) {
+			return true
+		}
+	}
+	return false
+}
+
+func (p *Parser) isAtEnd() bool {
+	return p.peek() == TOKEN_EOF
+}
+
+func (p *Parser) peek() string {
+	return p.tokens[p.tokensIndex].TokenType
+}
 
 func Parse(lexTokens []LexToken) ([]Expr, []error) {
-	var topExpr Expr
+	parser := Parser{tokens: lexTokens, tokensIndex: 0}
+	var exprs []Expr
 	var errs []error
 
-
-
-	return []Expr{topExpr}, errs
+	for !parser.isAtEnd() {
+		expr, err := parseExpression(&parser)
+		if err != nil {
+			errs = append(errs, err)
+		}
+		if expr != nil {
+			exprs = append(exprs, expr)
+		}
+	}
+	return exprs, errs
 }
 
+func parseExpression(parser *Parser) (Expr, error) {
+	return parseEquality(parser)
+}
+
+func parseEquality(parser *Parser) (Expr, error) {
+	if false {
+	}
+
+	return parseComparison(parser)
+}
+
+func parseComparison(parser *Parser) (Expr, error) {
+	if false {
+	}
+
+	return parseTerm(parser)
+}
+
+func parseTerm(parser *Parser) (Expr, error) {
+	if false {
+	}
+
+	return parseFactor(parser)
+}
+
+func parseFactor(parser *Parser) (Expr, error) {
+	if false {
+	}
+
+	return parseUnary(parser)
+}
+
+func parseUnary(parser *Parser) (Expr, error) {
+	if parser.match(TOKEN_BANG, TOKEN_MINUS) {
+		parser.advance()
+		operator := parser.previous()
+		right, err := parseUnary(parser)
+		if err != nil {
+			return nil, err
+		}
+		return &ExprUnary{operator, &right}, nil
+	}
+	return parsePrimary(parser)
+}
+
+func parsePrimary(parser *Parser) (Expr, error) {
+	if parser.match(TOKEN_NUMBER, TOKEN_STRING, TOKEN_TRUE, TOKEN_FALSE, TOKEN_NIL) {
+		parser.advance()
+		return &ExprLiteral{parser.previous()}, nil
+	} else if parser.check(TOKEN_LEFT_PAREN) {
+		parser.advance()
+		expr, err := parseExpression(parser)
+		if err != nil {
+			parser.advance()
+			return nil, err
+		}
+		if expr == nil {
+			parser.advance()
+			return nil, nil
+		}
+		if parser.check(TOKEN_RIGHT_PAREN) {
+			parser.advance()
+			return &ExprGroup{expr}, nil
+		}
+	}
+	return nil, fmt.Errorf("Error: unmatched parenthesis")
+}
