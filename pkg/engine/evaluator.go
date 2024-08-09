082131597647154e9dcc838e13f20a77a238f8ea
wezik <wezik@users.noreply.github.com>
2024-08-09 16:02:56 +0200

    Empty decimal trimming

--- pkg/engine/evaluator.go
+++ pkg/engine/evaluator.go
@@ -1,12 +1,25 @@
 package engine
 
+import (
+	"strconv"
+)
+
 func Evaluate(ast []Expr) (string, []error) {
 	stringAST := ""
 	var errs []error
 	for _, expr := range ast {
 		if expr, ok := expr.(*ExprLiteral); ok {
-			stringAST += expr.String()
+			if expr.LexToken.TokenType == TOKEN_NUMBER {
+				stringAST += trimEmptyDecimal(expr.String())
+			} else {
+				stringAST += expr.String()
+			}
 		}
 	}
 	return stringAST, errs
+}
+
+func trimEmptyDecimal(s string) string {
+	f, _ := strconv.ParseFloat(s, 64)
+	return strconv.FormatFloat(f, 'f', -1, 64)
 }
